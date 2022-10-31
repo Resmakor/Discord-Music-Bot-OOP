517bae37b596f6723f68ead2bcd30764abac58c3
Bartłomiej Czajka <52079500+Resmakor@users.noreply.github.com>
2022-10-31 21:11:57 +0100

    Update messages.py
    Disconnecting from voice channel after 60 seconds of being silent

--- cogs/messages.py
+++ cogs/messages.py
@@ -1,12 +1,13 @@
 import nextcord
 from nextcord.ext import commands
 import validators
+import asyncio
 
 class Messages(commands.Cog):
     def __init__(self, client):
         self.client = client
         self.bot_prefix = client.command_prefix
-
+      
     @commands.Cog.listener()
     async def on_ready(self):
         """Function changes bot status to: listening Young Leosia - Szklanki when bot is online"""
@@ -24,6 +25,23 @@ class Messages(commands.Cog):
                         await message.channel.send('Xd is not an emote!')
                     else:
                         await message.channel.send(f'Haha {word}')
+                      
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member, before, after):
+        """Bot disconnects after 60 seconds of being silent"""
+      if not member.id == self.client.user.id:
+          return
+      
+      elif before.channel is not None:
+          return
+      
+      else:
+          voice = after.channel.guild.voice_client
+          while True:
+              await asyncio.sleep(60)
+              if voice.is_playing() == False and voice.is_paused() == False:
+                  await voice.disconnect()
+                  break    
 
 def setup(client):
     client.add_cog(Messages(client))
